4e5e1af27066b0667e25ca76fef5829660fc5efc
Kazuki Horie <kazukihorie.jp@gmail.com>
2020-01-19 01:55:59 +0900

    Use GET: statuses/home_timeline

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -1,2 +1,25 @@
 class TwitterController < ApplicationController
+  TWITTER_API_KEY    = ENV['TWITTER_API_KEY']
+  TWITTER_API_SECRET = ENV['TWITTER_API_SECRET']
+
+  ACCESS_TOKEN        = ENV['TWITTER_ACCESS_TOKEN']
+  ACCESS_TOKEN_SECRET = ENV['TWITTER_ACCESS_TOKEN_SECRET']
+
+  def home_timeline
+    consumer = OAuth::Consumer.new(
+      TWITTER_API_KEY,
+      TWITTER_API_SECRET,
+      site: 'https://api.twitter.com/'
+    )
+
+    endpoint = OAuth::AccessToken.new(
+      consumer,
+      session[:access_token],
+      session[:access_token_secret]
+    )
+
+    response = endpoint.get('https://api.twitter.com/1.1/statuses/home_timeline.json')
+
+    @tweets = JSON.parse(response.body)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
 
+  get 'twitter/home_timeline', to: 'twitter#home_timeline'
+
   namespace :oauth do
     get 'twitter/', to: 'twitter#authorize'
     get 'twitter/access_token', to: 'twitter#access_token'
